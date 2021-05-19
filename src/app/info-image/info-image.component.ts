ce45e0a0f415412f10f48668211679e5614e187b
Patrick Fisher <patrick.fisher9999@gmail.com>
2021-05-19 01:15:40 -0500

    add profile page and nav bar

---
+++ src/app/info-image/info-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-image',
+  templateUrl: './info-image.component.html',
+  styleUrls: ['./info-image.component.css']
+})
+export class InfoImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
